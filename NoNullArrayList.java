3981368e4bfc1f2c69e4c6cbfbb60b88831f671c
jason687 <>
2020-12-14 20:39:10 -0500

    add methods

---
+++ NoNullArrayList.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class NoNullArrayList<T> extends ArrayList<T> {
+  public T value;
+
+  public NoNullArrayList () {
+    super();
+  }
+
+  public NoNullArrayList (int a) {
+    super(a);
+  }
+
+  public boolean add (T thing) {
+    if (thing == null) {
+      throw new IllegalArgumentException ("No nulls!");
+    } else {
+      super.add(thing);
+    }
+    return true;
+  }
+
+  public void add (int index, T thing) {
+    if (thing == null) {
+      throw new IllegalArgumentException ("No nulls!");
+    } else {
+      super.add(index, thing);
+    }
+  }
+}
